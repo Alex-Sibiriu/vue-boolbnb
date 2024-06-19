5ebd846bf6b816edf5eb655b7a90a3f04d1158ee
Alessandro-Moroni <alemoro98@gmail.com>
2024-06-19 14:57:49 +0200

    [did]card-vue file

---
+++ src/components/partials/Card.vue
@@ -0,0 +1,25 @@
+<script>
+   export default {
+    name: 'Footer',
+
+     
+  }
+</script>
+    
+<template>
+
+
+  <div class="container my-3">
+
+    <h1>Footer</h1>
+    
+
+
+  </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
