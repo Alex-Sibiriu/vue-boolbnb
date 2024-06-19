7d530a34860a889e3077ba765e63621cec3b7aaa
EmilianaManzo <emiliana.ma96@gmail.com>
2024-06-19 18:15:22 +0200

    [add] Service.vue component

--- src/components/partials/partialsHome/Services.vue
+++ src/components/partials/partialsHome/Services.vue
@@ -1,15 +1,90 @@
 <script>
+  import { store } from '@/data/store';
+  import axios from 'axios';
   export default {
-    
+    data(){
+      return{
+        store,
+        favServices : []
+      }
+    },
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl + 'services')
+            .then(result =>{
+              console.log(result.data);
+              store.services = result.data 
+              this.getFavServices();
+            })
+            .catch(errors =>{
+              console.log(errors);
+            })
+      },
+      getFavServices(){
+        this.favServices = store.services.filter(service => {
+          return service.name === 'Cene di Gala' || 
+            service.name === 'Mostre d\'Arte' ||
+            service.name === 'Spettacoli Teatrali' ||
+            service.name === 'Rievocazioni Storiche' ||
+            service.name === 'Concerti' ||
+            service.name === 'Maneggio' 
+        });
+        console.log(this.favServices)
+        return this.favServices
+      }
+    },
+    mounted(){
+      this.getApi();
+
+      
+    }
   }
 </script>
 <template>
-  <div>
-
+  <div class="container ">
+    <h1 class="text-center fw-bold my-5">Services </h1>
+    <div class="row">
+      <div class="col">
+        <ul class="columns">
+          <li v-for="service in favServices" :key="service.id"><i class="icon me-5" :class="service.icon"></i> {{ service.name }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
+@use '../../../assets/scss/main.scss' as * ;
+.container{
+  box-shadow: 5px 5px 12px 0px $shadow-color;
+  h1{
+    color: $brand-color;
+  }
+  .row{
+    .col{
+      .columns{
+        list-style: none;
+        columns: 2;
+          li{
+            margin-bottom: 30px;
+            font-size: 1.3rem;
+            font-weight: bolder;
+            
+            .icon{
+              font-size: 2rem ;
+            }
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width:768px){
+  .columns{
+    columns: 1 !important;
+    margin-left: 20%;
+  }
+}
 
 </style>
