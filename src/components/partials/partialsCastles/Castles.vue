a7d64310cc1d421bfc53bd357977eeb7d4985df2
Alessandro-Moroni <alemoro98@gmail.com>
2024-06-20 15:38:03 +0200

    "Updated Castles.vue and Card.vue with new CSS styles, added Navbar component to Houses.vue"

--- src/components/partials/partialsCastles/Castles.vue
+++ src/components/partials/partialsCastles/Castles.vue
@@ -15,10 +15,10 @@ import {store} from '../../../data/store.js';
   <div class="row row-cols-4 pb-5">
     <div class="col mb-5" v-for="castle in store.houses" :key="castle">
 
-      <div class="castle" >
+      <div class="castle h-100 rounded-2" >
         <img src="https://siviaggia.it/wp-content/uploads/sites/2/2021/09/Castello-di-Neuschwanstein.jpg" alt="">
-        <h4>{{ castle.title }}</h4>
-        <p>{{ castle.address }}</p>
+        <h4 class="p-2">{{ castle.title }}</h4>
+        <p class="ps-2">{{ castle.address }}</p>
       </div>
 
     </div>
@@ -32,6 +32,8 @@ import {store} from '../../../data/store.js';
 
 .castle{
   width: 300px;
+  overflow: hidden;
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.7);
   img{
     width: 100%;
     object-fit: cover;
---
+++ src/components/partials/partialsHome/Navbar.vue
@@ -0,0 +1,177 @@
+<script>
+  import _ from 'lodash';
+
+  import {store} from '../../../data/store.js';
+  import axios from 'axios';
+
+  export default {
+    name: 'Navbar',
+    components: {
+      
+    },
+    setup() {
+      return {
+        
+      };
+    },
+
+    data() {
+      return {
+        store,
+        suggestions: [],
+        selectedAddress: false
+      }
+    },
+
+    methods: {
+      getSuggestions: _.debounce(function() {
+      if (store.inputAddress.length > 1) {
+        axios.get(`https://api.tomtom.com/search/2/search/${store.inputAddress}.json`, {
+            params: {
+              key: 'sOhLhoDd5uQFkArKiM1liP4BtoqFAxAk',
+              typeahead: true,
+              language: 'it-IT',
+              countrySet: 'IT',
+              limit: 10
+            }
+          })
+          .then(response => {
+            this.suggestions = response.data.results.map(suggestion => suggestion.address.freeformAddress);
+          })
+          .catch(error => {
+            console.error('Errore nella ricerca degli indirizzi suggeriti: ', error);
+          });
+      } else {
+        this.suggestions = [];
+      }
+    }, 300),
+
+    selectSuggestion(suggestion) {
+      store.inputAddress = suggestion;
+      this.selectedAddress = true;
+      this.suggestions = [];
+    },
+
+    submitForm() {
+      if (this.selectedAddress) {
+        this.suggestions = [];
+        this.$router.push({ name: 'houses' });
+      } else {
+        alert('Per favore seleziona un indirizzo valido dai suggerimenti.');
+      }
+    },
+
+    handleCastles(event) {
+      const input = this.$el.querySelector('input');
+      const suggestionsBox = this.$el.querySelector('.suggestions');
+      
+      if (
+        
+        !this.$el.contains(event.target) &&
+        !input.contains(event.target) &&
+        !suggestionsBox.contains(event.target)
+      ) {
+        this.suggestions = [];
+      }
+    },
+
+    getFilteredCastles() {
+        this.loading = true
+        if (store.inputAddress != '') {
+          axios.get(store.apiUrl + 'houses/search/' + store.inputAddress + '/300')
+            .then(response => {
+              store.houses = response.data;
+              this.loading = false
+            })
+            .catch(error => {
+              console.error('Errore nella ricerca dei castelli vicini: ', error);
+              this.loading = false
+            })
+        } else {
+          this.getApi();
+        }
+      }
+    },
+
+    mounted() {
+      document.addEventListener('click', this.handleCastles);
+    },
+
+    beforeDestroy() {
+      document.removeEventListener('click', this.handleCastles);
+    },
+  };
+</script>
+
+<template>
+  <div class="position-relative" >
+    
+
+    
+    <div class="searchbar  w-50">
+      <form class="form-inline" @submit.prevent="getFilteredCastles">
+
+        <div class="position-relative">
+          <div class="d-flex">
+            <input class="form-control rounded-end-0 rounded-bottom-0 w-100" type="text" placeholder="Ricerca per luogo"
+              aria-label="Search" v-model="store.inputAddress" @keyup="selectedAddress = false, getSuggestions()">
+              <div class="btn-box">
+                <button class="rounded-end-3 rounded-bottom-0 px-2" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+              </div>
+          </div>
+            <div class="suggestions position-absolute w-100 rounded-bottom-2 overflow-hidden">
+              <p
+                v-for="(suggestion, index) in suggestions"
+                :key="`sug-${index}`"
+                @click.stop="selectSuggestion(suggestion)"
+                class="bg-white p-1 ps-3 m-0 border-bottom border-secondary-subtle">{{ suggestion }}</p>
+            </div>
+        </div>
+      </form>
+    </div>
+
+  </div>
+
+
+
+</template>
+
+<style lang="scss" scoped>
+@import '../../../assets/scss/partials/variables.scss';
+
+
+
+
+/* Aggiungi stile al form */
+.searchbar {
+
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.7);
+
+  
+  h2{
+    color: #eaeaea;
+    margin-bottom: 30px;
+  }
+  button[type='submit']{
+    background: white;
+    color: rgb(5, 28, 44) ;
+    align-content: center;
+    border: none;
+    width: 100%;
+    height: 100%;
+  }
+
+  .suggestions {
+    cursor: pointer;
+  }
+}
+
+
+
+
+@media screen and (max-width: 768px) {
+  .searchbar {
+    width: 90% !important;
+  }
+}
+</style>
