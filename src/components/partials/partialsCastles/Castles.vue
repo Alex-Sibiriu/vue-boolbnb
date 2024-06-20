76702112f570e75a2fa3d7a6cf3afc26a6d42f95
Alessandro-Moroni <alemoro98@gmail.com>
2024-06-20 18:28:11 +0200

    "Wrapped castle component with router-link and added styles for link"

--- src/components/partials/partialsCastles/Castles.vue
+++ src/components/partials/partialsCastles/Castles.vue
@@ -15,11 +15,14 @@ import {store} from '../../../data/store.js';
   <div class="row  pb-5 row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 justify-content-center">
     <div class="col mb-5 px-4 justify-content-center d-flex" v-for="castle in store.houses" :key="castle">
 
-      <div class="castle h-100 rounded-2" >
-        <img src="https://siviaggia.it/wp-content/uploads/sites/2/2021/09/Castello-di-Neuschwanstein.jpg" alt="">
-        <h4 class="p-2">{{ castle.title }}</h4>
-        <p class="ps-2">{{ castle.address }}</p>
-      </div>
+      <router-link :to="{name: 'houseDetails', params:{slug: castle.slug}}" >
+
+        <div class="castle h-100 rounded-2" >
+          <img src="https://siviaggia.it/wp-content/uploads/sites/2/2021/09/Castello-di-Neuschwanstein.jpg" alt="">
+          <h4 class="p-2">{{ castle.title }}</h4>
+          <p class="ps-2">{{ castle.address }}</p>
+        </div>
+      </router-link>
 
     </div>
   </div>
@@ -38,6 +41,10 @@ import {store} from '../../../data/store.js';
     width: 100%;
     object-fit: cover;
   }
+}
+a{
+  text-decoration: none;
+  color: $dark-color;
 }
 
 
